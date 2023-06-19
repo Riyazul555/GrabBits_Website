8c11c905a6155c2e3481a4e2ae7028d1f666d2eb
Md Riyazul Islam <121575277+Riyazul555@users.noreply.github.com>
2023-06-19 22:58:47 +0530

    Updated email validation

--- client/src/helper/validate.js
+++ client/src/helper/validate.js
@@ -92,7 +92,7 @@ function emailVerify(error ={}, values){
     }else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)){
         error.email = toast.error("Invalid email address...!")
     }
-    else if(values.email.length() < 0 ){
+    else if(values.email.length() > 6 ){
         error.email = toast.error("Invalid email address...!")
     }
     else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/i.test(values.email)){
